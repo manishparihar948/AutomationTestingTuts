6c34554355cfd4bcf2628190572624ec0118b626
manishparihar948 <manish.parihar948@gmail.com>
2024-09-05 17:26:55 +0200

    updated config.py

--- myenvOrangeHRM/configuration/config.py
+++ myenvOrangeHRM/configuration/config.py
@@ -1,7 +1,8 @@
 class TestData:
     BROWSER = 'chrome'
     CHROME_EXECUTABLE_PATH = "drivers/chromedriver_mac"
-    # FIREFOX_EXECUTABLE_PATH = "Drivers/geckodriver_mac"
+    
+    FIREFOX_EXECUTABLE_PATH = "Drivers/geckodriver_mac"
 
 
     URL = "https://opensource-demo.orangehrmlive.com/"
